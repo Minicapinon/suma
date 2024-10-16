17ffdd1b0f2b46389255e80deb48145f3505e633
Minicapinon <bava080502hgtrtna6@cbtis217.edu.mx>
2024-10-16 08:33:23 +0200

    Added logic to the program

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
+double oper(double var1, double var2);
+
 int main()
 {
-	std::"Hello World!";
+	double var1,var2;
+	std::cin>>var1;
+	std::cout<<'\n';
+
+	std::cin>>var2;
+	std::cout<<'\n';
+
+	double res = oper(var1,var2);
+
+	std::cout<<res;
 
 	return 0;
+}
+
+double oper(double var1, double var2)
+{
+	return var1 + var2;
 }
